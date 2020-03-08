4c8ad63fa39b7603be15704a406512eb0dd9290e
billyedwards3017 <43846656+billyedwards3017@users.noreply.github.com>
2020-03-08 21:23:00 +0000

    Add project files.

---
+++ CMP1124MAlgorithmsAssignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CMP1124MAlgorithmsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //trying to add this to the repo
+        }
+    }
+}
