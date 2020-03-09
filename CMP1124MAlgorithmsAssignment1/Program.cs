7806d943aabf6cd7a34877dda6d7ca6b58126230
billyedwards3017 <43846656+billyedwards3017@users.noreply.github.com>
2020-03-09 12:32:14 +0000

    Added code at workshop

--- CMP1124MAlgorithmsAssignment1/Program.cs
+++ CMP1124MAlgorithmsAssignment1/Program.cs
@@ -16,7 +16,8 @@ namespace CMP1124MAlgorithmsAssignment1
             
             Console.WriteLine("Would you like to work with log 1 (1), log 2 (2) or log 3 (3)");
 
-                int ans = Convert.ToInt32(Console.ReadLine());
+            int ans = Convert.ToInt32(Console.ReadLine());
+
             string[] Net = new string[255]; 
             do
             {
@@ -39,22 +40,52 @@ namespace CMP1124MAlgorithmsAssignment1
 
                 }
             } while (ans != 1 && ans != 2 && ans != 3);
+            //This is the code which allows the user to choose which string they would like the code to interact with
+
 
             int[] NetInt = Array.ConvertAll(Net, int.Parse);
-            for (int log = 0; log < NetInt.Length; log++)
-            {
-                Console.WriteLine(NetInt[log]);
-                Console.ReadKey();
-            }
-
-
+            //This line converts the string array of the inputted text file into a integer array of the same data for the sorting algorithm
+            int[] SortedNetInt = new int[255];
+                QuickSort(NetInt);
+            //This line is calling the Quick Sort algorithm on the integer array.
         }
 
-        public static void QuickSort(int [] data)
+        public static void QuickSort(int[] data)
         {
+            QuickSort(data, 0, data.Length - 1);
+            Console.WriteLine(data[0]);
+        }
 
+        public static void QuickSort(int [] data, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = data[(left + right)/2];
+            int temp;
 
+            do
+            {
+                while ((data[i] < pivot) && (i < right)) i++;
+                while ((pivot < data[j]) && (j > left)) j--;
 
+                if (i <= j)
+                {
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (left < j)
+            {
+                QuickSort(data, left, j);
+            }
+            if (i < right)
+            {
+                QuickSort(data, i, right);
+            }
         }
     }
 }
